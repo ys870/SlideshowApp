1d718e8a31ef991771b28658bdfba29d0469ebde
Y.S <y.s.87870@gmail.com>
2020-08-11 15:42:11 +0900

    スライドショーアプリを作成
    進む、戻るボタンで画像表示する機能を実装

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -10,6 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBAction func prevImage(_ sender: Any) {
+        
+        imageNumber -= 1
+        showImage()
+    }
+    
+    
+    @IBAction func nextImage(_ sender: Any) {
+        imageNumber += 1
+        showImage()
+    }
+    
+    var imageNumber = 0
+    
+    func showImage() {
+        
+        let imageArray = ["aosagi", "hasibosogarasu", "kawasemi", "kogera", "yurikamome"]
+        
+        if imageNumber < 0 {
+            imageNumber = 4
+        }
+        
+        if imageNumber > 4 {
+            imageNumber = 0
+        }
+
+        
+        let name = imageArray[imageNumber]
+        let image = UIImage(named: name)
+        
+        imageView.image = image
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
